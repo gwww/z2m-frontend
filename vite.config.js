cc6de3257354ae09d9458c2175564cc7b1e361e5
Glenn Waters <glenn@watrs.ca>
2023-02-04 21:26:53 -0500

    Disable unocss/icons (for now) breaks build. No icons in app at the moment.

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import UnoCSS from 'unocss/vite';
-import presetIcons from '@unocss/preset-icons';
+// import UnoCSS from 'unocss/vite';
+// import presetIcons from '@unocss/preset-icons';
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [
+		// UnoCSS({
+		// 	presets: [
+		// 		presetIcons({
+		// 			extraProperties: {
+		// 				'display': 'inline-block',
+		// 				'vertical-align': 'middle',
+		// 			},
+		// 		}),
+		// 	],
+		// }),
 		sveltekit(),
-		UnoCSS({
-			presets: [
-				presetIcons({
-					extraProperties: {
-						'display': 'inline-block',
-						'vertical-align': 'middle',
-					},
-				}),
-			],
-		}),
 	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
