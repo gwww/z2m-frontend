fee34b80cd49bed42b0faf5da80ae236fc645d7c
Glenn Waters <glenn@watrs.ca>
2023-02-07 17:07:17 -0500

    Moving unocss to client only remove runtime errors. Temp fix.

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,6 +1,7 @@
 import { MQTT_handler } from '$lib/mqtt';
 import { browser } from '$app/environment';
 import { mqtt_env } from './mqtt_env.js'; // Private file with credentials in it
+import 'uno.css';
 
 if (browser) {
   new MQTT_handler(mqtt_env.server, {
