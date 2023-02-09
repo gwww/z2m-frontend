fb0ec54ec986ebe9ad3c1adaa6d6ffd5d92c218e
Glenn Waters <glenn@watrs.ca>
2023-02-09 13:20:28 -0500

    Add helper for case conversions.

--- src/lib/utils/case.ts
+++ src/lib/utils/case.ts
@@ -1,3 +1,5 @@
+import type { Dictionary } from '$lib/types'
+
 // Adapted from: https://stackoverflow.com/a/39718708
 export const camel2Title = (str: string) => str
   .replace(/([A-Z0-9])/g, (match) => ` ${match}`)
@@ -9,5 +11,10 @@ export const snake2Title = (str: string) => str
   .replace(/^[-_]*(.)/, (_, c) => c.toUpperCase())
   .replace(/[-_]+(.)/g, (_, c) => ' ' + c.toUpperCase())
 
-export const any2Title = (str: string) => str
-  .includes('_') ? camel2Title(str) : snake2Title(str)
+const exceptions: Dictionary<string> = {
+  'linkquality': 'Link Quality',
+}
+
+export const any2Title = (str: string) => {
+  exceptions[str] ? exceptions[str] : str.includes('_') ? camel2Title(str) : snake2Title(str)
+}
