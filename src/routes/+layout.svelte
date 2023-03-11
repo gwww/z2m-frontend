3d2b8879635117e9143d8c20011e8daaa4aef8db
Glenn Waters <glenn@watrs.ca>
2023-03-10 23:11:57 -0500

    Tidy. Not using modal, so removed.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,14 +4,11 @@
     import '../app.postcss';
     import { AppShell, AppBar } from '@skeletonlabs/skeleton';
     import { LightSwitch } from '@skeletonlabs/skeleton';
-    import { Modal } from '@skeletonlabs/skeleton';
+
     import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
     import { storePopup } from '@skeletonlabs/skeleton';
     storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
-
-<!-- This is a global definition, i.e.: not an instantiation. -->
-<Modal />
 
 <!-- App Shell -->
 <AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
