472671498b3144f199c0bd6e30fd5a3f927e826d
Glenn Waters <glenn@watrs.ca>
2023-03-11 20:08:31 -0500

    Remove utils directory

--- src/lib/utils/feature.ts
+++
@@ -1,5 +0,0 @@
-import { AccessType, type ExposedItemBase } from '$lib/types';
-
-export const isWritable = (feature: ExposedItemBase): boolean => {
-    return !feature.access || (feature.access & AccessType.ACCESS_WRITE) != 0
-}
--- src/lib/utils/generateId.js
+++
@@ -1,5 +0,0 @@
-let n = Date.now();
-
-export default function (prefix = 'tx-') {
-    return prefix + (++n).toString(36);
-}
